39f1d8ae55359f011a66bc1c7b30dab439b3269f
Sha <lolitacoding@gmail.com>
2025-05-10 20:17:49 +0800

    fix: 修复签到系统更换至新签到系统

--- Readme.md
+++ Readme.md
@@ -7,17 +7,20 @@
 #### 免责声明
 本文件仅供学习和技术交流使用，不得用于非法用途。对于因滥用而导致的任何法律责任，本人概不负责。
 
-#### 环境要求:
-    requests
-    beautifulsoup4
-
 #### 使用说明
-1. 修改main.py的最后的 `if__name__=="__main__":` 函数:
-替换其中的 `用户名/邮箱` 和 `密码`
-```python
-if __name__ == "__main__":
-    # 用户名-密码登录
-    AutoSign("用户名", "密码").start()
-    # 邮箱-密码登录
-    AutoSign("邮箱", "密码", is_email = True).start()
+1. 直接运行
+```shell
+    # 进入main.py所在目录
+    
+    # 安装依赖库
+    pip install -r requirements.txt
+    
+    # 命令行运行
+    python main.py -u "用户" -p "密码" -m "登录方式"
+    
+    # 示例1 用户名-密码
+    python main.py -u "用户名" -p "密码"
+    
+    # 示例2 邮箱-密码
+    python main.py -u "邮箱" -p "密码" -m "email"
 ```
--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-import datetime
 import requests
 from bs4 import BeautifulSoup
 import hashlib
-
-
-def get_now():
-    return datetime.datetime.now().strftime("%y/%m/%d %H:%M:%S")
-
+import argparse
 
 class AutoSign:
     # 登录界面 (提取loginHash)
@@ -16,7 +11,7 @@ class AutoSign:
     # 签到地址
     _sign_url = "https://www.jkju.cc/plugin.php"  # GET
     # 签到页面
-    _sign_page_url = "https://www.jkju.cc/plugin.php?id=k_misign:sign" # GET
+    _sign_page_url = "https://www.jkju.cc/plugin.php?id=zqlj_sign" # GET
 
     _login_form_data = {"referer": "https://www.jkju.cc/", "questionid": 0, "answer": "", "cookietime": "2592000"}
 
@@ -36,17 +31,17 @@ class AutoSign:
     }
 
     def __init__(self, username, password, is_email=False):
-        self._rank = None
         self._session = requests.session()
         self._username = username
         self._password = hashlib.md5(password.encode()).hexdigest()
         self._login_form_data["username"] = username
         self._login_form_data["password"] = password
+        self._sign_page_html = None
         if is_email:
             self._login_form_data["loginfield"] = "email"
         else:
             self._login_form_data["loginfield"] = "username"
-        self.message = f"镜客居签到\n账号:{username}\n"
+        self.message = f"签到任务: 镜客居\n登录账号: {username}\n"
 
     def _get_login_hash(self):
         html = self._session.get(url=self._login_page, params={"mod": "logging", "action": "login"}).text
@@ -63,14 +58,14 @@ class AutoSign:
         self._login_header["cookie"] = self._solve_cookie()
         login_response = self._session.post(self._login_url, params=self._login_params, data=self._login_form_data,
                                             headers=self._login_header).text
+        if "请输入验证码继续登录" in login_response:
+            return 0
         if "欢迎您回来" in login_response:
-            return True
-        return False
+            return 1
+        return -1
 
     def _get_sign_hash(self):
-        self._sign_header["cookie"] = self._solve_cookie()
-        html = self._session.get(url=self._sign_url, params={"id": "k_misign:sign"}, headers=self._sign_header).text
-        soup = BeautifulSoup(html, 'html.parser')
+        soup = BeautifulSoup(self._sign_page_html, 'html.parser')
         form_tag = soup.find("form", {"id": "scbar_form"})
         sign_form_hash = form_tag.find("input", {"name": "formhash", "type": "hidden"}).get("value")
         return sign_form_hash
@@ -78,21 +73,28 @@ class AutoSign:
     def sign(self):
         sign_hash = self._get_sign_hash()
         self._sign_header["cookie"] = self._solve_cookie()
-        response = self._session.get(url=self._sign_url, headers=self._sign_header,params={"id": "k_misign:sign", "operation": "qiandao", "format": "empty", "inajax": 1, "ajaxtarget": "JD_sign", "formhash": sign_hash}).text
-        success = self.check_sign()
-        if "<![CDATA[今日已签]]>" in response:
+        response = self._session.get(url=self._sign_url, headers=self._sign_header,params={"id": "zqlj_sign", "sign": sign_hash}).text
+        if response.find("恭喜您，打卡成功！") >= 0:
+            return 1
+        if response.find("您今天已经打过卡了，请勿重复操作！") >= 0:
             return 0
-        else:
-            if success:
-                return 1
-            return -1
+        return -1
 
-    def check_sign(self):
-        page_html = self._session.get(url=self._sign_page_url).text
-        bs = BeautifulSoup(page_html, 'html.parser')
-        qd_rank = bs.find("input", attrs={"id": "qiandaobtnnum"}).get("value")
-        if int(qd_rank) > 0:
-            self._rank = qd_rank
+    def _init_sign_page(self):
+        self._sign_page_html = self._session.get(url=self._sign_page_url).text
+
+    def _get_sign_trend(self):
+        soup = BeautifulSoup(self._sign_page_html, 'lxml')
+        trend_lis = soup.select('#wp > div.ct2.cl > div.sd > div:nth-of-type(3) > div.bm_c > ul > li')
+        trend_message = ""
+        for li in trend_lis:
+            trend_message += f'{li.text}\n'
+        return trend_message
+
+    def _check_sign(self):
+        bs = BeautifulSoup(self._sign_page_html, 'html.parser')
+        sign_status_text = bs.find("div", attrs={"class": "bm signbtn cl"}).find("a").text
+        if sign_status_text.find("今日已打卡") >= 0:
             return True
         return False
 
@@ -104,23 +106,37 @@ class AutoSign:
 
     def start(self):
         login_status = self.login()
-        if not login_status:
-            self.message += f"{get_now()}\n状态:登录失败\n"
+
+        if login_status == 0:
+            self.message += "登录状态: 频繁登录，需要验证码\n"
+        if login_status == -1:
+            self.message += "登录状态: 登录失败\n"
             print(self.message)
             return
-        sign_status = self.sign()
-        if sign_status == 0:
-            self.message += f"{get_now()}\n状态:今日已经签到\n签到排名:{self._rank}\n"
-        elif sign_status == 1:
-            self.message += f"{get_now()}\n状态:签到成功\n签到排名:{self._rank}\n"
+
+        self._init_sign_page()
+        has_sign = self._check_sign()
+        if has_sign:
+            self.message += f"执行结果: 今日已签到\n"
+            self.message += self._get_sign_trend()
         else:
-            self.message += f"{get_now()}\n状态:签到失败\n"
+            sign_status = self.sign()
+            if sign_status == -1:
+                self.message += f'执行结果: 签到失败\n'
+            else:
+                self.message += f'执行结果: {"签到成功" if sign_status == 1 else "今日已签到"}\n'
+                self._init_sign_page()
+                self.message += self._get_sign_trend()
         print(self.message)
 
 
 if __name__ == "__main__":
-    # 任选一种方式进行
-    AutoSign("用户名", "密码").start()
-    AutoSign("邮箱", "密码", is_email=True).start()
-
-
+    args_parser = argparse.ArgumentParser()
+    args_parser.add_argument("-u","--user", type=str, required=True, help="用户")
+    args_parser.add_argument("-p","--password", type=str, required=True, help="密码")
+    args_parser.add_argument("-m","--mode", type=str, help="模式: username | email ")
+    arguments = args_parser.parse_args()
+    if arguments.mode is None or arguments.mode.strip().lower() != "email":
+        arguments.mode = "username"
+    signer = AutoSign(arguments.user, arguments.password, arguments.mode.strip().lower() == 'email')
+    signer.start()
