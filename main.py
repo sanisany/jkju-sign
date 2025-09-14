55c779a636c04fad08ae56d6bcd41d4c3bec306c
sloved <lolitacoding@gmail.com>
2025-09-14 11:06:53 +0800

    fix(login): 修复登录问题

--- main.py
+++ main.py
@@ -53,15 +53,14 @@ class AutoSign:
         self.message = f"签到任务: 镜客居\n登录账号: {username}\n"
 
     def _get_login_hash(self) -> tuple[str, str]:
-        # 第一次 (403)，但设置了临时 cookie
-        self.session.get(
+        resp = self.session.get(
             self.LOGIN_PAGE, params={"mod": "logging", "action": "login"}
         )
-        # 第二次 (200)，必须带上第一次的 cookie
-        resp2 = self.session.get(
-            self.LOGIN_PAGE, params={"mod": "logging", "action": "login"}
-        )
-        soup = BeautifulSoup(resp2.text, "html.parser")
+        if resp.status_code == 403:
+            resp = self.session.get(
+                self.LOGIN_PAGE, params={"mod": "logging", "action": "login"}
+            )
+        soup = BeautifulSoup(resp.text, "html.parser")
 
         form_tag = soup.find("form", {"name": "login"})
         formhash = form_tag.find("input", {"name": "formhash", "type": "hidden"}).get("value")
@@ -75,22 +74,21 @@ class AutoSign:
         self.login_form_data["formhash"] = formhash
         self.LOGIN_PARAMS["loginhash"] = loginhash
 
-        self.session.post(
-            self.LOGIN_URL,
-            params=self.LOGIN_PARAMS,
-            data=self.login_form_data,
-            headers=self.LOGIN_HEADERS,
-        )
-
-        # 登录前清理掉临时 cookie
-        self.session.cookies.clear_expired_cookies()
-
         resp = self.session.post(
             self.LOGIN_URL,
             params=self.LOGIN_PARAMS,
             data=self.login_form_data,
             headers=self.LOGIN_HEADERS,
         )
+        if resp.status_code == 403:
+            self.session.cookies.clear_expired_cookies()
+
+            resp = self.session.post(
+                self.LOGIN_URL,
+                params=self.LOGIN_PARAMS,
+                data=self.login_form_data,
+                headers=self.LOGIN_HEADERS,
+            )
 
         text = resp.text
         if "请输入验证码继续登录" in text:
